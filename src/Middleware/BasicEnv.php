3cf38823f7902d40ffcb10abbc79632a0602a389
Temur5319436 <temur5319436@gmail.com>
2024-06-03 15:22:00 +0500

    feat: basic auth's middleware is added

--- src/Middleware/BasicEnv.php
+++ src/Middleware/BasicEnv.php
@@ -2,6 +2,38 @@
 
 namespace Usmonaliyev\EnvAuth\Middleware;
 
-class BasicEnv {
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
+class BasicEnv
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, \Closure $next)
+    {
+        if ($this->validate($request)) {
+            return $next($request);
+        }
+
+        return Response::make(['message' => 'Invalid credentials'], 401, ['WWW-Authenticate' => 'Basic']);
+    }
+
+    protected function validate(Request $request): bool
+    {
+        return $this->validUser($request->getUser()) && $this->validPassword($request->getPassword());
+    }
+
+    protected function validUser(?string $username): bool
+    {
+        return $username !== null && $username === config('env-auth.basic.username');
+    }
+
+    protected function validPassword(?string $password): bool
+    {
+
+        return $password !== null && $password === config('env-auth.basic.password');
+    }
 }
